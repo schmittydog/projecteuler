2d93a75455a4b857045c3ec34e53bbdeff5e5e2b
Stephen Dunn <sdunn@stephendunn.com>
2021-12-25 21:45:26 -0800

    Add files

--- 12/12.go
+++ 12/12.go
@@ -16,7 +16,7 @@ func main() {
 	peuler.PrimesTo(1000000)
 	for i := 2; i < 1000000; i++ {
 		divCount := 1
-		triNum := (i*i+i)/2
+		triNum := (i*i + i) / 2
 		for _, tup := range peuler.GetPrimeTups(triNum) {
 			divCount *= tup[1] + 1
 		}
