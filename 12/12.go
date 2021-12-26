30b96cbc8c7d9bfe85bfa4094c7f93cf084b6851
Stephen Dunn <sdunn@stephendunn.com>
2021-12-25 18:49:32 -0800

    iads

---
+++ 12/12.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/schmittydog/projecteuler/peuler"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	peuler.PrimesTo(1000000)
+	primes := peuler.GetPrimeArray(1000000)
+	for i := 2; i < 1000000; i++ {
+		divCount := 1
+		triNum := (i*i+i)/2
+		for _, tup := range peuler.GetPrimeTups(triNum) {
+			divCount *= tup[1] + 1
+		}
+		if divCount > 500 {
+			fmt.Println(divCount)
+			break
+		}
+	}
+}
---
+++ 12/go.mod
@@ -0,0 +1,7 @@
+module github.com/schmittydog/projecteuler/12
+
+go 1.17
+
+require github.com/schmittydog/projecteuler/peuler v0.0.0-20211226023902-9a10a9d0466e
+
+require github.com/schmittydog/projecteuler/bitsieve v0.0.0-20211226015229-e61e3c12574d // indirect
