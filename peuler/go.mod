d88a50deff54bdf3f2190d4a62540b3a33ac6123
Stephen Dunn <sdunn@stephendunn.com>
2021-12-25 17:46:04 -0800

    Added Peuler utilities

---
+++ peuler/go.mod
@@ -0,0 +1,5 @@
+module github.com/schmittydog/projecteuler/peuler
+
+go 1.17
+
+require github.com/schmittydog/projecteuler/bitsieve v0.0.0-20211226001819-7ebb9e349bab
---
+++ peuler/peuler.go
@@ -0,0 +1,71 @@
+package peuler
+
+import (
+	"math/big"
+
+	"github.com/schmittydog/projecteuler/bitsieve"
+)
+
+var (
+	BS     *bitsieve.BitSieve
+	Primes []int
+)
+
+func PrimesTo(n int) {
+	BS = bitsieve.NewBitSieve(n)
+	Primes = BS.GetArray(n)
+}
+
+func IsPrime(n int) bool {
+	if n <= N && BS.IsIn(n) {
+		return true
+	}
+	bn := big.NewInt(int64(n))
+	return bn.ProbablyPrime(15)
+}
+
+func GetPrimeFactors(n int) []int {
+	factors := []int{}
+	for _, p := range Primes {
+		if p*p > n {
+			break
+		}
+		if n%p == 0 {
+			factors = append(factors, p)
+		}
+		for n%p == 0 {
+			n /= p
+		}
+	}
+	if n > 1 {
+		factors = append(factors, n)
+	}
+	return factors
+}
+
+func PowMod(a, b, p int) int {
+	a %= p
+	if a == 0 {
+		return 0
+	}
+	product := 1
+	for b > 0 {
+		if b&1 == 1 {
+			product *= a
+			product %= p
+			b--
+		}
+		a *= a
+		a %= p
+		b >>= 1
+	}
+	return product
+}
+
+func InvPowMod(a, p int) int {
+	return PowMod(a, p-2, p)
+}
+
+func CombiMod(fact_n, fact_k, fact_n_k, p int) int {
+	return ((fact_n * InvPowMod(fact_k, p) % p) * InvPowMod(fact_n_k, p)) % p
+}
